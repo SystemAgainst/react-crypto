41c9d9f6dfb896588aaf9b96f57f020a4907a625
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-07-19 10:15:17 +0300

    ref: add ts modal props

--- src/components/CoinInfoModel.tsx
+++ src/components/CoinInfoModel.tsx
@@ -1,7 +1,16 @@
-export default function CoinInfoModel({ coin }) {
+import { Crypto } from '../types/ICrypto.ts';
+import React from 'react';
+
+interface CoinInfoModelProps {
+  coin: Crypto | null;
+}
+
+const CoinInfoModel: React.FC<CoinInfoModelProps> = ({ coin }) => {
   return (
     <>
-      <h2>{coin.name}</h2>
+      <h2>{coin?.name}</h2>
     </>
   );
-}
+};
+
+export default CoinInfoModel;
