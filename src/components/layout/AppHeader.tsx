a68c480d4058d31a40dcc7b89053c648edcb832f
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-07-19 09:30:11 +0300

    upd: style for header and process data

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -1,15 +1,38 @@
-import { Layout } from 'antd';
+import { Button, Layout, Select, Space } from 'antd';
 import React from 'react';
+import { useCrypto } from '../../context/crypto-context.tsx';
 
 const headerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
+  width: '100%',
+  padding: '1rem',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 };
 
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>;
+  const { crypto } = useCrypto();
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{ width: '250px' }}
+        value="press / to open"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img style={{ width: 20 }} src={option.data.icon} alt={option.data.label} />
+            {option.data.label}
+          </Space>
+        )}
+      />
+
+      <Button type="primary">Add asset</Button>
+    </Layout.Header>
+  );
 }
