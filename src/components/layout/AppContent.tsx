7704b59ca9c6b47eab994ec28e8f354614cec10a
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-07-23 16:10:39 +0300

    add: summarize all actives

--- src/components/layout/AppContent.tsx
+++ src/components/layout/AppContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../../context/crypto-context.tsx';
 
 const contentStyle: React.CSSProperties = {
   textAlign: 'center',
@@ -10,5 +11,22 @@ const contentStyle: React.CSSProperties = {
 };
 
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+  return (
+    <>
+      <Layout.Content style={contentStyle}>
+        <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+          Portfolio: {' '}
+          {
+            assets.map((asset) => {
+              const coin = crypto.find((c) => c.id === asset.id);
+              return asset.amount * coin.price;
+            })
+              .reduce((acc, v) => (acc + v), 0)
+              .toFixed(2)
+          }$
+        </Typography.Title>
+      </Layout.Content>
+    </>
+  );
 }
